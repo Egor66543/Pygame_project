eb6fb51296434396d9286dd1850f3f8830bd5482
PR <grafinyabox@yandex.ru>
2022-12-24 17:04:44 +0300

    commit 4

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ import os
 import sys
 
 pygame.init()
+pygame.font.init()
+
+con = sqlite3.connect("game_words.db")
+cur = con.cursor()
+id_word = cur.execute(f'''select id from words where id={random.randint(1, 10)}''').fetchone()[0]
+word = cur.execute(f'''SELECT word FROM words WHERE id={id_word}''').fetchone()[0]
+description = cur.execute(f'''SELECT description FROM words WHERE id={id_word}''').fetchone()[0]
+res = id_word, word, description
 
 
 def load_image(name, colorkey=None):
@@ -28,35 +36,39 @@ class Board:
         self.screen = pygame.display.set_mode((h, w))
         self.a = 50
 
-    def db(self):
-        con = sqlite3.connect("game_words.db")
-        cur = con.cursor()
-        id_word = cur.execute(f'''select id from words where id={random.randint(1, 10)}''').fetchone()[0]
-        word = cur.execute(f'''SELECT word FROM words WHERE id={id_word}''').fetchone()[0]
-        res = id_word, word
-        return res
-
     def render(self):
         k = 0
-        for x in range(len(self.db()[1])):
+        for x in range(len(res[1])):
             pygame.draw.rect(self.screen, 'white', (50 + k, 400, self.a, self.a), 1)
             k += self.a
 
-    def hint(self):
-        pass
+    def description(self):
+        font1 = pygame.font.Font(None, 24)
+        text = font1.render(res[2], True, 'white')
+        board.screen.blit(text, (50, 500))
 
 
 board = Board(800, 700)
 board.render()
 board.render()
+all_sprites = pygame.sprite.Group()
+lamp = pygame.sprite.Sprite(all_sprites)
+lamp.image = load_image("data/lamp.jpg")
+lamp.rect = lamp.image.get_rect()
+lamp.mask = pygame.mask.from_surface(lamp.image)
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        x, y = pygame.mouse.get_pos()
+        if event.type == pygame.MOUSEBUTTONDOWN and (0 <= x <= 50 and 0 <= y <= 51):
+            board.description()
 
+    all_sprites.draw(board.screen)
     pygame.display.flip()
+
 pygame.quit()
 
 
